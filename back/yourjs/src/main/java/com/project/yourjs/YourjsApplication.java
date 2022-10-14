13c66c8907470a2015ad7ae59ce84c6413b8c9ed
송형근 <soo1095@naver.com>
2022-10-14 16:57:05 +0900

    back: Spring Project Init

---
+++ back/yourjs/src/main/java/com/project/yourjs/YourjsApplication.java
@@ -0,0 +1,13 @@
+package com.project.yourjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourjsApplication.class, args);
+    }
+
+}
