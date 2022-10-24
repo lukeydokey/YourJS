1a9f7b2e4d73619c4c733f5535417395bd5cda4e
백승진 <mixkio@naver.com>
2022-10-24 17:16:56 +0900

    feat:jwt notice CRUD done

--- back/yourjs/src/main/java/com/project/yourjs/api/service/NoticeService.java
+++ back/yourjs/src/main/java/com/project/yourjs/api/service/NoticeService.java
@@ -13,67 +13,88 @@ import com.project.yourjs.api.res.NoticeDeleteRes;
 import com.project.yourjs.api.res.NoticePatchRes;
 import com.project.yourjs.api.res.NoticePostRes;
 import com.project.yourjs.db.entity.Notice;
+import com.project.yourjs.db.entity.User;
 import com.project.yourjs.db.repository.NoticeRepository;
+import com.project.yourjs.db.repository.UserRepository;
 
 @Service
 public class NoticeService {
     private final NoticeRepository noticeRepository;
+    private final UserRepository userRepository;
 
-    public NoticeService(NoticeRepository noticeRepository) {
+    public NoticeService(NoticeRepository noticeRepository, UserRepository userRepository) {
         this.noticeRepository = noticeRepository;
+        this.userRepository = userRepository;
     }
-    public List<Notice> getAllNotice(){
+
+    public List<Notice> getAllNotice() {
         return noticeRepository.findAll();
     }
 
     @Transactional
-    public NoticePostRes createNotice(String userName, NoticeReq noticeReq){
+    public NoticePostRes createNotice(String userId, NoticeReq noticeReq) {
         NoticePostRes noticePostRes = new NoticePostRes();
+        noticePostRes.setResult("fail");
         Notice notice = new Notice();
-        notice.setUserName(userName);
-        notice.setNoticeName(noticeReq.getNoticeName());
-        notice.setLink(noticeReq.getLink());
-        notice.setProgress(noticeReq.getProgress());
-        notice.setModDtm(LocalDateTime.now());
-        notice.setRegDtm(LocalDateTime.now());
-        notice = noticeRepository.save(notice);
-        if(notice != null){noticePostRes.setResult("success");}
-        else{noticePostRes.setResult("fail");}
+        Optional<User> oUser = userRepository.findByUserId(userId);
+        if (oUser.isPresent()) {
+            User user = oUser.get();
+            notice.setUser(user);
+            notice.setNoticeName(noticeReq.getNoticeName());
+            notice.setLink(noticeReq.getLink());
+            notice.setProgress(noticeReq.getProgress());
+            notice.setModDtm(LocalDateTime.now());
+            notice.setRegDtm(LocalDateTime.now());
+            notice = noticeRepository.save(notice);
+            if (notice != null) {
+                noticePostRes.setResult("success");
+            }
+        }
         return noticePostRes;
     }
 
     @Transactional
-    public NoticePatchRes updateNotice(String userName, Integer noticeSeq, NoticeReq noticeReq){
+    public NoticePatchRes updateNotice(String userId, Integer noticeSeq, NoticeReq noticeReq) {
         NoticePatchRes noticePatchRes = new NoticePatchRes();
         noticePatchRes.setResult("fail");
-        Optional<Notice> oNotice = noticeRepository.findById(noticeSeq);
-        if(oNotice.isPresent()){
-            Notice notice = oNotice.get();
-            if(StringUtils.isNotBlank(noticeReq.getNoticeName()))
-                notice.setNoticeName(noticeReq.getNoticeName());
-            if(StringUtils.isNotBlank(noticeReq.getLink()))
-                notice.setLink(noticeReq.getLink());
-            if(StringUtils.isNotBlank(noticeReq.getProgress()))
-                notice.setProgress(noticeReq.getProgress());
-            notice.setModDtm(LocalDateTime.now());
-            notice = noticeRepository.save(notice);
-            if(notice != null){
-                noticePatchRes.setResult("success");
+        Optional<User> oUser = userRepository.findByUserId(userId);
+        if (oUser.isPresent()) {
+            User user = oUser.get();
+            Optional<Notice> oNotice = noticeRepository.findById(noticeSeq);
+            if (oNotice.isPresent()) {
+                Notice notice = oNotice.get();
+                if (notice.getUser().getUserSeq() != user.getUserSeq())
+                    return noticePatchRes;
+                if (StringUtils.isNotBlank(noticeReq.getNoticeName()))
+                    notice.setNoticeName(noticeReq.getNoticeName());
+                if (StringUtils.isNotBlank(noticeReq.getLink()))
+                    notice.setLink(noticeReq.getLink());
+                if (StringUtils.isNotBlank(noticeReq.getProgress()))
+                    notice.setProgress(noticeReq.getProgress());
+                notice.setModDtm(LocalDateTime.now());
+                notice = noticeRepository.save(notice);
+                if (notice != null) {
+                    noticePatchRes.setResult("success");
+                }
             }
         }
-        return noticePatchRes; 
+        return noticePatchRes;
     }
 
     @Transactional
-    public NoticeDeleteRes deleteNotice(String userName, Integer noticeSeq){
+    public NoticeDeleteRes deleteNotice(String userId, Integer noticeSeq) {
         NoticeDeleteRes noticeDeleteRes = new NoticeDeleteRes();
         noticeDeleteRes.setResult("fail");
-        Optional<Notice> oNotice = noticeRepository.findById(noticeSeq);
-        if(oNotice.isPresent()){
-            Notice notice = oNotice.get();
-            if(notice.getUserName().equals(userName)){
-                noticeRepository.deleteById(noticeSeq);
-                noticeDeleteRes.setResult("success");
+        Optional<User> oUser = userRepository.findByUserId(userId);
+        if (oUser.isPresent()) {
+            User user = oUser.get();
+            Optional<Notice> oNotice = noticeRepository.findById(noticeSeq);
+            if (oNotice.isPresent()) {
+                Notice notice = oNotice.get();
+                if (notice.getUser().getUserSeq()==user.getUserSeq()) {
+                    noticeRepository.deleteById(noticeSeq);
+                    noticeDeleteRes.setResult("success");
+                }
             }
         }
         return noticeDeleteRes;
--- back/yourjs/src/main/java/com/project/yourjs/db/entity/Notice.java
+++ back/yourjs/src/main/java/com/project/yourjs/db/entity/Notice.java
@@ -7,10 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,7 +33,10 @@ public class Notice {
   @Column(name = "notice_seq", nullable = false, columnDefinition = "INT UNSIGNED UNIQUE")
   private Integer noticeSeq;
 
-  private String userName;
+  @JsonBackReference
+  @ManyToOne(targetEntity = User.class)
+  @JoinColumn(name = "userSeq")
+  private User user;
 
   @Column(name = "notice_name", length = 45)
   private String noticeName;
