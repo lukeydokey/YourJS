84cbc9fb3f92e48d0ebc7bf3aa297f28f8e7143e
김국진 <rnrwls0211@naver.com>
2022-10-25 11:58:31 +0900

    [S07P31E102-126]feat:front:소셜로그인카카오

---
+++ front/yourjs/src/pages/KakaoLogin.jsx
@@ -0,0 +1,13 @@
+// 카카오 로그인 시 Redirect URI
+// 이 페이지에서 백엔드에 인가코드를 보내주며 로그인 토큰을 발급받는다.
+
+import React from 'react';
+
+const KakaoLogin = () => {
+  // 리다이렉트 된 페이지에서 인가코드 꺼내오기
+  const code = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
+  return <div></div>;
+};
+
+export default KakaoLogin;
