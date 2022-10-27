ccf7db31934f5ae3ed95e46b2b34251967ef35e9
김국진 <rnrwls0211@naver.com>
2022-10-27 16:33:47 +0900

    [S07P31E102-127]아이디/닉네임중복체크/회원가입기능구현

---
+++ front/yourjs/src/common/color.js
@@ -0,0 +1,13 @@
+export const colors = {
+  // 확인 버튼 색상
+  buttonBlue: '#1785f2',
+  // 카카오 색상
+  kakao: '#f9e000',
+  // 네이버 색상
+  naver: '#2db400',
+
+  // 긍정 확인 메세지 색상
+  positiveMsgColor: 'blue',
+  // 부정 확인 메세지 색상
+  negativeMsgColor: 'red',
+};
