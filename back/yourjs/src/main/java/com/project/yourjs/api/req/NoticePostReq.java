86a10c55ec73c29cad74982b766e2c917c711367
백승진 <mixkio@naver.com>
2022-10-27 13:57:08 +0900

    hotfix:back: [user cors 예외 추가]

---
+++ back/yourjs/src/main/java/com/project/yourjs/api/req/NoticePostReq.java
@@ -0,0 +1,10 @@
+package com.project.yourjs.api.req;
+
+import lombok.Getter;
+
+@Getter
+public class NoticePostReq {
+    private String noticeName;
+    private String link;
+    private String progress;
+}
--- back/yourjs/src/main/java/com/project/yourjs/api/req/NoticeUpdateReq.java
+++ back/yourjs/src/main/java/com/project/yourjs/api/req/NoticeUpdateReq.java
@@ -3,7 +3,8 @@ package com.project.yourjs.api.req;
 import lombok.Getter;
 
 @Getter
-public class NoticeReq {
+public class NoticeUpdateReq {
+    private Integer noticeSeq;
     private String noticeName;
     private String link;
     private String progress;
