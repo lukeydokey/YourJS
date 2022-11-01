6f42de8e818a201bb9bf5dedadbe528bbeb50690
황인빈 <hibeen1@naver.com>
2022-11-01 18:02:46 +0900

    [S07P31E102-138] feat:back: S3 파일 관련 기능 구현 중

--- back/yourjs/src/main/java/com/project/yourjs/api/controller/S3Controller.java
+++ back/yourjs/src/main/java/com/project/yourjs/api/controller/S3Controller.java
@@ -1,13 +1,33 @@
-//package com.project.yourjs.api.controller;
-//
-//import com.project.yourjs.util.S3Uploader;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class S3Controller {
-//
-//    private final S3Uploader s3Uploader;
-//
-//}
+package com.project.yourjs.api.controller;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.project.yourjs.util.S3Uploader;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("aws")
+public class S3Controller {
+
+    private final S3Uploader s3Uploader;
+    private final AmazonS3Client amazonS3Client;
+
+    @PostMapping()
+    public String uploadFile(@RequestPart MultipartFile multipartFile) throws IOException {
+        s3Uploader.uploadFiles(multipartFile, "testhibeen1");
+        return "확인해보세요!";
+    }
+}
+
