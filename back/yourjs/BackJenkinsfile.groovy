7c1721eff6f85184dfde8189d37647a4cb5819be
송형근 <soo1095@naver.com>
2022-11-02 17:32:00 +0900

    [S07P31E102-139] fix:back: Jenkinsfile env파일 위치 수정

--- back/yourjs/BackJenkinsfile.groovy
+++ back/yourjs/BackJenkinsfile.groovy
@@ -22,41 +22,42 @@ podTemplate(label: 'builder',
         ],
         volumes: [
                 hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
-                hostPathVolume(mountPath: '/etc/spring/properties', hostPath: '/home/ubuntu/env'),
+                hostPathVolume(mountPath: '/etc/spring/env', hostPath: '/home/ubuntu/env'),
                 //hostPathVolume(mountPath: '/usr/bin/docker', hostPath: '/usr/bin/docker')
         ]) {
     node('builder') {
         stage('Start'){
             notifyStarted(DISCORD_CHANNEL);
         }
-        stage('Checkout') {
-            checkout scm   // gitlab으로부터 소스 다운
-            sh "cp -r /etc/spring/properties ./back/yourjs/src/main/resources/properties"
-        }
-        stage('Docker build') {
-            container('docker') {
-                withCredentials([usernamePassword(
-                        credentialsId: 'docker_hub_auth',
-                        usernameVariable: 'USERNAME',
-                        passwordVariable: 'PASSWORD')]) {
-                    /* 도커 빌드를 수행한다 */
-                    sh "docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAGS}-back ./back/yourjs"
-                    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-                    sh "docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAGS}-back"
+        try {
+            stage('Checkout') {
+                checkout scm   // gitlab으로부터 소스 다운
+                sh "cp /etc/spring/env/env.properties ./back/yourjs/src/main/resources/properties/env.properties"
+            }
+            stage('Docker build') {
+                container('docker') {
+                    withCredentials([usernamePassword(
+                            credentialsId: 'docker_hub_auth',
+                            usernameVariable: 'USERNAME',
+                            passwordVariable: 'PASSWORD')]) {
+                        /* 도커 빌드를 수행한다 */
+                        sh "docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAGS}-back ./back/yourjs"
+                        sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+                        sh "docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAGS}-back"
+                    }
                 }
             }
-        }
-        stage('Run kubectl') {
-            container('kubectl') {
-                withCredentials([usernamePassword(
-                        credentialsId: 'docker_hub_auth',
-                        usernameVariable: 'USERNAME',
-                        passwordVariable: 'PASSWORD')]) {
-                    /* namespace 존재여부 확인. 미존재시 namespace 생성 */
-                    sh "kubectl get ns ${NAMESPACE}|| kubectl create ns ${NAMESPACE}"
+            stage('Run kubectl') {
+                container('kubectl') {
+                    withCredentials([usernamePassword(
+                            credentialsId: 'docker_hub_auth',
+                            usernameVariable: 'USERNAME',
+                            passwordVariable: 'PASSWORD')]) {
+                        /* namespace 존재여부 확인. 미존재시 namespace 생성 */
+                        sh "kubectl get ns ${NAMESPACE}|| kubectl create ns ${NAMESPACE}"
 
-                    /* docker secret 존재여부 확인. 미존재시 secret 생성 */
-                    sh """
+                        /* docker secret 존재여부 확인. 미존재시 secret 생성 */
+                        sh """
                             kubectl get secret my-secret -n ${NAMESPACE} || \
                             kubectl create secret docker-registry my-secret \
                             --docker-server=https://index.docker.io/v1/ \
@@ -66,18 +67,21 @@ podTemplate(label: 'builder',
                             -n ${NAMESPACE}
                         """
 
-                    /* k8s-deployment.yaml 의 env값을 수정해준다(DATE로). 배포시 수정을 해주지 않으면 변경된 내용이 정상 배포되지 않는다. */
-                    /*sh "echo ${VERSION}"
-                    sh "sed -i.bak 's#VERSION_STRING#${VERSION}#' ./k8s/k8s-deployment.yaml"*/
-                    sh "echo ${DATE}"
-                    sh "sed -i.bak 's#DATE_STRING#${DATE}#' ./back/yourjs/k8s/yourjs-back-deployment.yaml"
+                        /* k8s-deployment.yaml 의 env값을 수정해준다(DATE로). 배포시 수정을 해주지 않으면 변경된 내용이 정상 배포되지 않는다. */
+                        /*sh "echo ${VERSION}"
+                        sh "sed -i.bak 's#VERSION_STRING#${VERSION}#' ./k8s/k8s-deployment.yaml"*/
+                        sh "echo ${DATE}"
+                        sh "sed -i.bak 's#DATE_STRING#${DATE}#' ./back/yourjs/k8s/yourjs-back-deployment.yaml"
 
-                    /* yaml파일로 배포를 수행한다 */
-                    sh "kubectl apply -f ./back/yourjs/k8s/yourjs-back-deployment.yaml -n ${NAMESPACE}"
-                    sh "kubectl apply -f ./back/yourjs/k8s/yourjs-back-service.yaml -n ${NAMESPACE}"
-                    sh "kubectl apply -f ./back/yourjs/k8s/yourjs-back-ingress.yaml"
+                        /* yaml파일로 배포를 수행한다 */
+                        sh "kubectl apply -f ./back/yourjs/k8s/yourjs-back-deployment.yaml -n ${NAMESPACE}"
+                        sh "kubectl apply -f ./back/yourjs/k8s/yourjs-back-service.yaml -n ${NAMESPACE}"
+                        sh "kubectl apply -f ./back/yourjs/k8s/yourjs-back-ingress.yaml"
+                    }
                 }
+
             }
+        }catch(Exception e){
             notifyResult(DISCORD_CHANNEL);
         }
     }
