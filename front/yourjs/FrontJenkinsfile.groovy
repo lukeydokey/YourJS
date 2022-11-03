28cad1e41db81f664d0d9e44aca6c10970162134
송형근 <soo1095@naver.com>
2022-11-03 09:14:14 +0900

    [S07P31E102-141] feat:front: Jenkins 프론트 자동배포 세팅

---
+++ front/yourjs/FrontJenkinsfile.groovy
@@ -0,0 +1,92 @@
+/* pipeline 변수 설정 */
+def DOCKER_IMAGE_NAME = "pudingles94/project"           // 생성하는 Docker repo 이름
+def DOCKER_IMAGE_TAGS = "yourjs"  // 생성하는 Docker image 태그
+def NAMESPACE = "ns-yourjs"
+def VERSION = "${env.BUILD_NUMBER}"
+def DATE = new Date();
+def DISCORD_CHANNEL = "https://discord.com/api/webhooks/1037265817961762867/1F_aOmpyEQlrAR31NutODRZmPBF-UFfaRKl4G4IsE7IniPlrofjOT2-XufAlKzZ7Gb6i";
+
+/* Discord 시작 알람 함수 */
+def notifyStarted(discord_channel) {
+    discordSend (webhookURL: "${discord_channel}", title: "${JOB_NAME}", result: "SUCCESS" , link: "${BUILD_URL}", description: "Build Started")
+}
+/* Discord 성공 알람 함수 */
+def notifySuccess(discord_channel) {
+    discordSend (webhookURL: "${discord_channel}", title: "${JOB_NAME}", result: "SUCCESS", link: "${BUILD_URL}", description: "Build Success")
+}
+/* Discord 실패 알람 함수 */
+def notifyFailure(discord_channel) {
+    discordSend (webhookURL: "${discord_channel}", title: "${JOB_NAME}", result: "FAILURE", link: "${BUILD_URL}", description: "Build Failed")
+}
+
+podTemplate(label: 'builder',
+        containers: [
+                containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true),
+                containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.25.0', command: 'cat', ttyEnabled: true)
+        ],
+        volumes: [
+                hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
+                hostPathVolume(mountPath: '/etc/react/env', hostPath: '/home/ubuntu/env'),
+                //hostPathVolume(mountPath: '/usr/bin/docker', hostPath: '/usr/bin/docker')
+        ]) {
+    node('builder') {
+        stage('Start'){
+            notifyStarted(DISCORD_CHANNEL);
+        }
+        try {
+            stage('Checkout') {
+                checkout scm   // gitlab으로부터 소스 다운
+                sh "cp -r /etc/react/env ./front/yourjs"
+            }
+            stage('Docker build') {
+                container('docker') {
+                    withCredentials([usernamePassword(
+                            credentialsId: 'docker_hub_auth',
+                            usernameVariable: 'USERNAME',
+                            passwordVariable: 'PASSWORD')]) {
+                        /* 도커 빌드를 수행한다 */
+                        sh "docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAGS}-front ./front/yourjs"
+                        sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+                        sh "docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAGS}-front"
+                    }
+                }
+            }
+            stage('Run kubectl') {
+                container('kubectl') {
+                    withCredentials([usernamePassword(
+                            credentialsId: 'docker_hub_auth',
+                            usernameVariable: 'USERNAME',
+                            passwordVariable: 'PASSWORD')]) {
+                        /* namespace 존재여부 확인. 미존재시 namespace 생성 */
+                        sh "kubectl get ns ${NAMESPACE}|| kubectl create ns ${NAMESPACE}"
+
+                        /* docker secret 존재여부 확인. 미존재시 secret 생성 */
+                        sh """
+                            kubectl get secret my-secret -n ${NAMESPACE} || \
+                            kubectl create secret docker-registry my-secret \
+                            --docker-server=https://index.docker.io/v1/ \
+                            --docker-username=${USERNAME} \
+                            --docker-password=${PASSWORD} \
+                            --docker-email=pudingles@gmail.com \
+                            -n ${NAMESPACE}
+                        """
+
+                        /* k8s-deployment.yaml 의 env값을 수정해준다(DATE로). 배포시 수정을 해주지 않으면 변경된 내용이 정상 배포되지 않는다. */
+                        /*sh "echo ${VERSION}"
+                        sh "sed -i.bak 's#VERSION_STRING#${VERSION}#' ./k8s/k8s-deployment.yaml"*/
+                        sh "echo ${DATE}"
+                        sh "sed -i.bak 's#DATE_STRING#${DATE}#' ./front/yourjs/k8s/yourjs-front-deployment.yaml"
+
+                        /* yaml파일로 배포를 수행한다 */
+                        sh "kubectl apply -f ./front/yourjs/k8s/yourjs-front-deployment.yaml -n ${NAMESPACE}"
+                        sh "kubectl apply -f ./front/yourjs/k8s/yourjs-front-service.yaml -n ${NAMESPACE}"
+                        sh "kubectl apply -f ./front/yourjs/k8s/yourjs-front-ingress.yaml"
+                    }
+                }
+                notifySuccess(DISCORD_CHANNEL);
+            }
+        }catch(Exception e){
+            notifyFailure(DISCORD_CHANNEL);
+        }
+    }
+}
