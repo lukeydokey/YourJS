79c51d91d7ab10f919e8df9b88bfb178e85070f1
김국진 <rnrwls0211@naver.com>
2022-11-07 13:41:55 +0900

    fix:front:카카오로그인수정

--- front/yourjs/src/pages/KakaoLogin.jsx
+++ front/yourjs/src/pages/KakaoLogin.jsx
@@ -2,12 +2,16 @@
 // 이 페이지에서 백엔드에 인가코드를 보내주며 로그인 토큰을 발급받는다.
 
 import React from 'react';
+import axios from 'axios';
+import { SERVER_IP, apis } from '../common/apis';
 
 const KakaoLogin = props => {
   // 리다이렉트 된 페이지에서 인가코드 꺼내오기
   const code = new URL(window.location.href).searchParams.get('code');
-  console.log(code);
-  console.log(props);
+  console.log(SERVER_IP + apis.kakaoLogin + `?code=${code}`);
+  axios
+    .get(SERVER_IP + apis.kakaoLogin + `?code=${code}`)
+    .then(response => console.log(response));
   return <div></div>;
 };
 
