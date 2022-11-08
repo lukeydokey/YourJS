0ff416f68a93565ab8c765a357b358a87fa69d05
김국진 <rnrwls0211@naver.com>
2022-11-08 09:33:36 +0900

    [S07P31E102-237]feat:front:네이버로그인구현

--- front/yourjs/src/pages/NaverLogin.jsx
+++ front/yourjs/src/pages/NaverLogin.jsx
@@ -3,6 +3,8 @@
 
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { SERVER_IP, apis } from '../common/apis';
+import axios from 'axios';
 
 const NaverLogin = () => {
   const location = useLocation();
@@ -10,7 +12,10 @@ const NaverLogin = () => {
   const getNaverToken = () => {
     if (!location.hash) return;
     const token = location.hash.split('=')[1].split('&')[0]; //token 출력
-    console.log(token);
+    axios
+      .get(SERVER_IP + apis.naverLogin + `?code=${token}`)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
 
   useEffect(() => {
