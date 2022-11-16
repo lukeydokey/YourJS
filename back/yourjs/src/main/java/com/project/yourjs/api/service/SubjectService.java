ddf5367aa0e61d460db1dcd6286c1234baf98525
황인빈 <hibeen1@naver.com>
2022-11-16 17:21:42 +0900

    [S07P31E102-225] feat:back: 포지션 겹치는 사람 공고 가져오는 API 수정

--- back/yourjs/src/main/java/com/project/yourjs/api/service/SubjectService.java
+++ back/yourjs/src/main/java/com/project/yourjs/api/service/SubjectService.java
@@ -1,13 +1,14 @@
 package com.project.yourjs.api.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.transaction.Transactional;
 
 import com.project.yourjs.api.res.Portfolio.Subject.SubjectUserRes;
+import com.project.yourjs.db.entity.Notice;
 import com.project.yourjs.db.entity.User;
 import com.project.yourjs.db.entity.UserSubject;
+import com.project.yourjs.db.repository.NoticeRepository;
 import com.project.yourjs.db.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,7 @@ public class SubjectService {
     private final SubjectRepository subjectRepository;
     private final UserSubjectRepository userSubjectRepository;
     private final UserRepository userRepository;
+    private final NoticeRepository noticeRepository;
 
 
     @Transactional
@@ -44,6 +46,11 @@ public class SubjectService {
             if (tempUser.getUserId().equals(userId)) {
                 continue;
             }
+            int cnt = noticeRepository.countAllByUser(tempUser);
+            System.out.println(cnt);
+            if (cnt == 0) {
+                continue;
+            }
             if (tempUser.getInfoLevel() != null && tempUser.getInfoLevel() < 3) {
                 SubjectUserRes subjectUserRes = new SubjectUserRes(
                         tempUser.getUserSeq(),
@@ -51,10 +58,28 @@ public class SubjectService {
                         tempUser.getUserName(),
                         tempUser.getNickname(),
                         tempUser.getInfoLevel(),
-                        tempUser.getUserImg()
+                        tempUser.getUserImg(),
+                        cnt
                 );
                 subjectUserResList.add(subjectUserRes);
             }
+        }
+        if (subjectUserResList.size() > 9) {
+            int MAX_SHOW = 9;
+            int i = 0;
+            Set set = new HashSet<Integer>();
+            while (true) {
+                if (set.size() >= MAX_SHOW) {
+                    break;
+                }
+                set.add((int) Math.random() * (subjectUserResList.size()-1));
+            }
+            List<SubjectUserRes> realRes = new ArrayList<>();
+            Iterator iter = set.iterator();
+            while (iter.hasNext()) {
+                realRes.add(subjectUserResList.get((int) iter.next()));
+            }
+            return realRes;
         }
         return subjectUserResList;
     }
--- back/yourjs/src/main/java/com/project/yourjs/db/repository/NoticeRepository.java
+++ back/yourjs/src/main/java/com/project/yourjs/db/repository/NoticeRepository.java
@@ -2,10 +2,12 @@ package com.project.yourjs.db.repository;
 
 import java.util.Optional;
 
+import com.project.yourjs.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.yourjs.db.entity.Notice;
 
 public interface NoticeRepository extends JpaRepository<Notice, Integer>{
     Optional<Notice> findByNoticeSeq(Integer noticeSeq);
+    int countAllByUser(User user);
 }
